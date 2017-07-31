cd690aecba4bed7fe0a1f58ed2498b878c71f5fa
Krishnasis Mandal <krishnasismandal97@gmail.com>
2017-07-31 18:31:00 +0530

    Update FixRope.cs

--- Assets/Scripts/FixRope.cs
+++ Assets/Scripts/FixRope.cs
@@ -4,11 +4,6 @@ using UnityEngine;
 
 public class FixRope : MonoBehaviour {
 
-	//public GameObject spark;
-	// Use this for initialization
-	void Start () {
-		
-	}
 	
 	void Update () {
 		transform.position=new Vector3(0f,-2.98f,7.66f);
